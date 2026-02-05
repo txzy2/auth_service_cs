941e97bcdf35d72c5edfc087369f77d0c99afe6d
txzy2 <txzy2@yandex.ru>
2026-02-06 02:58:05 +0300

    add logs

--- www/Program.cs
+++ www/Program.cs
@@ -2,6 +2,7 @@ using DotNetEnv;
 using MyMicroservice.API.Configuration;
 using MyMicroservice.Application.Configuration;
 using MyMicroservice.Infrastructure.Configuration;
+using Serilog;
 
 Env.Load();
 
@@ -13,6 +14,22 @@ builder.Services.AddRepositories();
 builder.Services.AddApplicationServices();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerDocumentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    var logPath = Path.Combine("logs", DateTime.Now.ToString("yyyy"), DateTime.Now.ToString("MM"), "info.log");
+
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .WriteTo.Console()
+        .WriteTo.File(
+            path: logPath,
+            rollingInterval: RollingInterval.Day,
+            rollOnFileSizeLimit: true,
+            retainedFileCountLimit: 31,
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .Enrich.FromLogContext();
+});
 
 var app = builder.Build();
 
